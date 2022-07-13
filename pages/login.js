a253ad1676bbc08ae6019cae31624e4604612ff1
Sang Hyun Han <hanbrandon@hotmail.com>
2022-07-13 16:02:28 -0700

    redirect

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,23 @@
 import React, { useRef, useEffect } from 'react';
-import { signIn, useSession, getCsrfToken } from 'next-auth/react';
+import { getSession, signIn, useSession, getCsrfToken } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-// import toast from 'react-hot-toast';
+import Router, { useRouter } from 'next/router';
+import toast, { Toaster } from 'react-hot-toast';
+import { useCookies } from 'react-cookie';
 
 const Login = () => {
 	const usernameRef = useRef();
 	const passwordRef = useRef();
+	const rememberRef = useRef();
 	const { data: session } = useSession();
 	const router = useRouter();
+	const [cookies, setCookie] = useCookies(['remember']);
+
+	const onRememberMeChange = () => {
+		setCookie('remember', rememberRef.current.checked);
+	};
 
 	const onSubmitHandler = async (e) => {
 		e.preventDefault();
@@ -18,26 +25,25 @@ const Login = () => {
 			redirect: false,
 			username: usernameRef.current.value,
 			password: passwordRef.current.value,
+			remember: rememberRef.current.checked,
 			callbackUrl: '/',
 		});
 
 		if (!result.error) {
-			router.push('/');
+			if (router.query.next) {
+				if (router.asPath.split('/login?next=/')[1] === '') {
+					Router.replace('/');
+				} else {
+					Router.replace(router.asPath.replace('/login?next=', ''));
+				}
+			} else {
+				Router.replace('/');
+			}
 		} else {
-			alert('Invalid username or password');
+			toast.error(result.error);
 		}
 	};
 
-	useEffect(() => {
-		console.log(session);
-		if (session) {
-			router.push('/');
-		}
-	}, [session]);
-
-	if (session) {
-		return <div>You already logged in</div>;
-	}
 	return (
 		<>
 			<Head>
@@ -47,17 +53,27 @@ const Login = () => {
 				<div className="sm:mx-auto sm:max-w-md text-center">
 					<Link href="/" passHref>
 						<a>
-							<Image width={64} height={56} src="/logo.png" alt="Exbon" />
+							<Image
+								width={64}
+								height={56}
+								src="/logo.png"
+								alt="Exbon"
+							/>
 						</a>
 					</Link>
-					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+						Sign in to your account
+					</h2>
 				</div>
 
 				<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 					<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 						<form className="space-y-6" onSubmit={onSubmitHandler}>
 							<div>
-								<label htmlFor="username" className="block text-sm font-medium text-gray-700">
+								<label
+									htmlFor="username"
+									className="block text-sm font-medium text-gray-700"
+								>
 									Username
 								</label>
 								<div className="mt-1">
@@ -74,7 +90,10 @@ const Login = () => {
 							</div>
 
 							<div>
-								<label htmlFor="password" className="block text-sm font-medium text-gray-700">
+								<label
+									htmlFor="password"
+									className="block text-sm font-medium text-gray-700"
+								>
 									Password
 								</label>
 								<div className="mt-1">
@@ -96,16 +115,23 @@ const Login = () => {
 										id="remember_me"
 										name="remember_me"
 										type="checkbox"
+										ref={rememberRef}
 										className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+										onChange={() => onRememberMeChange()}
 									/>
-									<label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+									<label
+										htmlFor="remember_me"
+										className="ml-2 block text-sm text-gray-900"
+									>
 										Remember me
 									</label>
 								</div>
 
 								<div className="text-sm">
 									<Link href="/forget-password" passHref>
-										<a className="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+										<a className="font-medium text-indigo-600 hover:text-indigo-500">
+											Forgot your password?
+										</a>
 									</Link>
 								</div>
 							</div>
@@ -124,6 +150,31 @@ const Login = () => {
 			</div>
 		</>
 	);
+};
+
+export const getServerSideProps = async (ctx) => {
+	const session = await getSession(ctx);
+	try {
+		if (session) {
+			return {
+				redirect: {
+					destination: `/`,
+					permanent: false,
+				},
+			};
+		} else {
+			return {
+				props: { session },
+			};
+		}
+	} catch {
+		return {
+			redirect: {
+				destination: '/error/403',
+				permanent: false,
+			},
+		};
+	}
 };
 
 export default Login;
